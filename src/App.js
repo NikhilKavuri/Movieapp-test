31da0bce1de571e9d337d1d543e5e5a4505825d1
Nikhilkavuri15 <nikhilkavuri1503@gmail.com>
2024-03-31 00:37:30 +0530

    Created the react project with structure

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          class="poster"
+          src="https://image.tmdb.org/t/p/w500/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg"
+          alt="Kung Fu Panda 4 Poster"
+        />
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
